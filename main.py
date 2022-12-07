243f92ce62595fd3d3e4e8fe2aabbc6dbb89c985
MomoBD <momoblydblk@gmail.com>
2022-12-06 21:14:17 -0500

    Add some notes on how to implement main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Procedures:
+1. Read thread number from config, or ask user for input
+2. Get the content of the thread
+3. Analyze the content of the thread
+4. Display the info that was extracted.
+"""
+
+
+def entry():
+    pass
+
+
+if __name__ == "__main__":
+    entry();
